7737cd0816d800b3fe1f1dfad8104b1ea88bf63e
AzizbekGulomov2002 <azizgulomov1529@gmail.com>
2023-01-25 15:46:37 +0500

    deploy-murojaat_bot

---
+++ config.py
@@ -0,0 +1,30 @@
+# Bot Data
+TOKEN = "5758408104:AAFDKmYAWgjBaDJ5g0nF98L9r8JPdneTavw"      # Get your bot token using https://t.me/BotFather
+
+# Support Chat
+CHAT_ID = '@QarMIIBujet_bot'                     # To find out your channels ID use: https://t.me/getidsbot
+'673320653'
+# Database Data
+HOSTNAME = "host_name"
+DATABASE = "db"
+USERNAME = "User_name"
+PORT_ID = 5432
+DB_PASS = "db_possword"
+
+# Predefined text to send, you can change its values to customize your own bot
+TEXT_MESSAGES = {
+    'start': 'Support Bot ga xush kelibsiz 👋 \n\n Iltimos, xabaringizni yuboring, biz sizning so`rovingizni ko`rib chiqamiz.',
+    'message_template': '<i>Message from: <b>@{0}</b>.</i>\n\n{1}<b>id: {2}</b>',
+    'is_banned': '❌ Foydalanuvchi taqiqlangan:' '✅ Foydalanuvchi muvaffaqiyatli bloklandi!',
+    'already_banned': '❌ Foydalanuvchi allaqachon taqiqlangan!', 'has_unbanned': '✅ Foydalanuvchi blokdan muvaffaqiyatli olib tashlandi!!',
+    'not_banned': '❌ Taqiqlanganlar ro`yxatida bunday foydalanuvchi yo`q!',
+    'user_banned': '🚫 Siz bu botga xabar yubora olmaysiz!',
+    'user_unbanned': '🥳 Siz aybsizligingizni isbotladingiz va endi bu botga yana yozishingiz mumkin!',
+    'user_reason_banned': '🚫 Sababi tufayli bu botga xabar yubora olmaysiz: <i>{}</i>.',
+    'pending': 'Iltimosingiz uchun rahmat! Biz allaqachon uni qayta ishlashga kirishdik.',
+    'unsupported_format': '❌ Sizning xabaringiz formati qo`llab-quvvatlanmaydi va u boshqa yo`naltirilmaydi.',
+    'message_not_found': '❌ Sizning xabaringiz bir kun oldin yuborilganga o`xshaydi. Tahrirlanadigan xabar topilmadi!',
+    'message_was_not_edited': '❌ Afsuski, siz rasm/videolarni o`zingiz tahrirlay olmaysiz.'
+                              'Iltimos, yangi xabar yuboring.',
+    'reply_error': '❌ Iltimos, faqat foydalanuvchi xabarlaridan yo`naltirilgan xabarlarga /ban yoki /unban bilan javob bering!'
+}
